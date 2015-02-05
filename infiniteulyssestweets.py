ea322033cccc2b6f4d42dc6677d1a9c3ce69e7b3
Ed Summers <ehs@pobox.com>
2015-02-05 11:24:31 -0500

    doc

--- infiniteulyssestweets.py
+++ infiniteulyssestweets.py
@@ -29,6 +29,8 @@ for entry in feed['entries']:
     target = re.split(':.', desc.select('.field-quote')[0].text, 1)[1]
     msg = '"%s" annotated by %s - %s' % (target, author, link)
     # tweepy silently ignores errors due to duplicate tweets and messages 
-    # that are too long ... which is nice
+    # that are too long ... which is nice, since it means we don't have 
+    # to keep a local database of some kind remembering which updates
+    # we have sent and which we haven't
     twtr.update_status(msg)
     time.sleep(10)
